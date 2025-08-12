1cc40f3c9d3653b72917fcd4a69f3eff65a9fe41
zzmthesurand <joshuabenzon@me.com>
2025-08-12 21:13:22 +1000

    cleaned up print

--- foodprint.py
+++ foodprint.py
@@ -3,21 +3,6 @@ from paperang_printer import Paperang_Printer  # your existing wrapper
 
 printer = Paperang_Printer()
 
-# Simple
-simple_label_to_png(
-    [
-        "Food: Chicken Thighs",
-        "Weight: 1.605 kg",
-        "Expiry: Aug 17, 2025",
-        "Frozen: Aug 12, 2025",
-    ],
-    "simple.png",
-    width_px=PRINTER_WIDTH,
-    bottom_margin_px=100,
-    threshold=180,
-)
-#printer.print_image_file("simple.png")
-
 # SVG
 svg_to_png_label_cairo(
     "food_template.svg",
@@ -27,14 +12,4 @@ svg_to_png_label_cairo(
     bottom_margin_px=10,
     threshold=180,
 )
-#printer.print_image_file("from_svg.png", rotation = 0)
-
-#printer.print_image_file("from_svg.png", rotation = 45)
-#printer.print_self_test()
-
-print("Printing image now")
-
-#printer.print_image_file("from_svg.png", padding=0, rotation = 90)
-#printer.print_image_file("from_svg.png", padding=0, density=70, rotation = 90)
-#printer.print_image_file("kumiko.jpg", padding=0, density=70, rotation = 90)
-printer.print_dithered_image("jonluke.jpg") #, rotation=90)
+printer.print_image_file("from_svg.png", rotation = 90)
